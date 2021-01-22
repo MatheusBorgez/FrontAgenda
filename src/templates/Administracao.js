bb2ba9cd1a656dfea34a5cbde0768c4848b7f3a0
Sameck Zanela <mrzanela@users.github.com>
2021-01-22 10:27:27 -0300

    injeção do html da tela de admin no template

--- src/templates/Administracao.js
+++ src/templates/Administracao.js
@@ -1,72 +1,76 @@
 exports.render = () => {
-    return ` <div class="container ">
-    <div>
-        <span class="login100-form-title p-b-43 p-2 mt-2">
-            Área Administrativa
-        </span>
-    </div>
+    return `  <div class="img-fluid text-right botaoShutdown mr-5 mt-5 text-white">
+    <a href="#"><img src="./images/shutdown.png" alt=""></a>
+    <strong class="mr-1">Sair</strong>
 </div>
+<div>
+    <div class="container ">
+        <span class="login100-form-title p-b-43 ">
+             Área Administrativa
+       </span>
+    </div>
 
-<div class="container ">
 
-            <div class="row ">
-                <div class="col-sm">
-                    <div id="divBusca" class="busca">
-                        <input type="text" id="txtBusca" placeholder="Buscar..." />
-                        <a class="" href="#">
-                            <img src="./images/pesquisa.png" id="btnBusca" alt="Buscar" />
-                        </a>
-                    </div>
+
+    <div class="container ">
+
+        <div class="row ">
+            <div class="col-sm">
+                <div id="divBusca" class="busca">
+                    <input type="text" id="txtBusca" placeholder="Buscar..." />
+                    <a class="" href="#">
+                        <img src="./images/pesquisa.png" id="btnBusca" alt="Buscar" />
+                    </a>
                 </div>
             </div>
+        </div>
 
-            <div class="row  border border-white back-grid text-white">
-                <div class="col-sm text-center">
-                    Nome
-
-                </div>
-                <div class="col-sm text-center">
-                    CPF
-                </div>
-                <div class="col-sm text-center">
-                    Matrícula
-                </div>
-            </div>
-
-            <div class="row back-gridrow1 text-dark">
-                <div class="col-sm">
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input mt-4" id="exampleCheck1">
-                    </div>
-                    <label class="text-center mb-2">Nome do Aluno</label>
-                </div>
-
-                <div class="col-sm ">
-                    <label class="text-center mt-3">___.___.___-__</label>
-                </div>
-
-                <div class="col-sm ">
-                    <label class="text-center mt-3">202101151247</label>
-                </div>
+        <div class="row  border border-white back-grid text-white">
+            <div class="col-sm text-center">
+                Nome
 
             </div>
-
-            <div class="row back-gridrow2 text-dark">
-                <div class="col-sm">
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input mt-4" id="exampleCheck1">
-                    </div>
-                    <label class="text-center mb-2">Nome do Aluno</label>
-                </div>
-
-                <div class="col-sm ">
-                    <label class="text-center mt-3">___.___.___-__</label>
-                </div>
-
-                <div class="col-sm ">
-                    <label class="text-center mt-3">202101151247</label>
-                </div>
-
+            <div class="col-sm text-center">
+                CPF
             </div>
-</div>`
+            <div class="col-sm text-center">
+                Matrícula
+            </div>
+        </div>
+
+        <div class="row back-gridrow1 text-dark">
+            <div class="col-sm">
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input mt-4" id="exampleCheck1">
+                </div>
+                <label class="text-center mb-2">Nome do Aluno</label>
+            </div>
+
+            <div class="col-sm ">
+                <label class="text-center mt-3">___.___.___-__</label>
+            </div>
+
+            <div class="col-sm ">
+                <label class="text-center mt-3">202101151247</label>
+            </div>
+
+        </div>
+
+        <div class="row back-gridrow2 text-dark">
+            <div class="col-sm">
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input mt-4" id="exampleCheck1">
+                </div>
+                <label class="text-center mb-2">Nome do Aluno</label>
+            </div>
+
+            <div class="col-sm ">
+                <label class="text-center mt-3">___.___.___-__</label>
+            </div>
+
+            <div class="col-sm ">
+                <label class="text-center mt-3">202101151247</label>
+            </div>
+
+        </div>`
 }
